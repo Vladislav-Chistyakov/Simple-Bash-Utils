10537c43371a20100279ee0162b1b4f917b4c6a8
Chas Tarred <chastarr@un-d6.kzn.21-school.ru>
2022-11-06 18:55:06 +0300

    added division into functions

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+void funcFlagS(int *testingFlags, int *stringNew, char simb, int numberSimbol);
+void funcFlagN(int numberSimbol, int flagS, int *number, int examS,
+               char oldSimb);
+void funcFlagB(int numberSimbol, int flagS, int *number, int examS,
+               char oldSimb, char simb);
+
+void funcFlagE(int flagS, char simb, int examS);
+
+void funcFlagT(char simb, int *examT);
+
+void funcFlagV(char simb, int *examV);
+
+void outputFuncCat(int flagS, int *examS, int *examV, int *examT, char simb);
 
 int main(int argc, char *argv[]) {
   int val;
@@ -83,105 +96,123 @@ int main(int argc, char *argv[]) {
 
       // отработка флага S
       if (flagsCat.flag_s) {
-        if (string_new == 1 && numberSimbol == 1) {
-          string_new = 2;
-          examS = 1;
-        }
-        if (string_new < 3) {
-          examS = 1;
-          if (simb != '\n') string_new = 0;
-        } else if (simb != '\n') {
-          string_new = 0;
-          examS = 1;
-        }
+        funcFlagS(&examS, &string_new, simb, numberSimbol);
       }
-
       // отработка флага N
       if (flagsCat.flag_n && !flagsCat.flag_b) {
-        if ((oldSimb == '\n' || numberSimbol == 1) && !flagsCat.flag_s) {
-          printf("%6.d\t", number);
-          number++;
-        } else if (flagsCat.flag_s && (oldSimb == '\n' || numberSimbol == 1)) {
-          if (examS) {
-            printf("%6.d\t", number);
-            number++;
-          }
-        }
+        funcFlagN(numberSimbol, flagsCat.flag_s, &number, examS, oldSimb);
       }
-
       // отработка флага B
       if (flagsCat.flag_b) {
-        // printf("%c", simb);
-        if (!flagsCat.flag_s && (numberSimbol == 1 || oldSimb == '\n') &&
-            simb != '\n') {
-          printf("%6.d\t", number);
-          number++;
-        }
-        if (flagsCat.flag_s && (oldSimb == '\n' || numberSimbol == 1)) {
-          if (simb != '\n' && examS) {
-            printf("%6.d\t", number);
-            number++;
-          }
-        }
+        funcFlagB(numberSimbol, flagsCat.flag_s, &number, examS, oldSimb, simb);
       }
-
       // отработка флага E
       if (flagsCat.flag_e) {
-        if (!flagsCat.flag_s && simb == '\n') {
-          printf("$");
-        } else if (examS && simb == '\n') {
-          printf("$");
-        }
+        funcFlagE(flagsCat.flag_s, simb, examS);
       }
-
       // отработка флага t
-
       if (flagsCat.flag_t) {
-        if (simb == 9) examT = 1;
+        funcFlagT(simb, &examT);
       }
-
       // отработка флага V
-
       if (flagsCat.flag_v) {
-        if (simb >= 0 && simb != 9 && simb != 10 && simb < 32) {
-          printf("^%c", simb + 64);
-          examV = 1;
-        } else if (simb == 127) {
-          printf("^?");
-          examV = 1;
-        }
+        funcFlagV(simb, &examV);
       }
-
       // ВЫВОД В КОНСОЛЬ
-      if (flagsCat.flag_s && examS) {
-        if (examT) {
-          printf("^I");
-          examS = 0;
-          examT = 0;
-        } else if (examV) {
-          examV = 0;
-        } else {
-          printf("%c", simb);
-          examS = 0;
-        }
-      } else if (!flagsCat.flag_s) {
-        if (examT) {
-          printf("^I");
-          // printf("I");
-          examT = 0;
-        } else if (examV) {
-          examV = 0;
-        } else {
-          printf("%c", simb);
-        }
-      }
+      outputFuncCat(flagsCat.flag_s, &examS, &examV, &examT, simb);
+      // взаимодействие с символами и строками
       oldSimb = simb;
       numberSimbol++;
     }
-
     fclose(fp);
     optind++;
   }
-  
   return 0;
+}
+
+void funcFlagS(int *testingFlags, int *stringNew, char simb, int numberSimbol) {
+  if (*stringNew == 1 && numberSimbol == 1) {
+    *stringNew = 2;
+    *testingFlags = 1;
+  }
+  if (*stringNew < 3) {
+    *testingFlags = 1;
+    if (simb != '\n') *stringNew = 0;
+  } else if (simb != '\n') {
+    *stringNew = 0;
+    *testingFlags = 1;
+  }
+}
+
+void funcFlagN(int numberSimbol, int flagS, int *number, int examS,
+               char oldSimb) {
+  if ((oldSimb == '\n' || numberSimbol == 1) && !flagS) {
+    printf("%6.d\t", *number);
+    *number = *number + 1;
+  } else if (flagS && (oldSimb == '\n' || numberSimbol == 1)) {
+    if (examS) {
+      printf("%6.d\t", *number);
+      *number = *number + 1;
+    }
+  }
+}
+
+void funcFlagB(int numberSimbol, int flagS, int *number, int examS,
+               char oldSimb, char simb) {
+  if (!flagS && (numberSimbol == 1 || oldSimb == '\n') && simb != '\n') {
+    printf("%6.d\t", *number);
+    *number = *number + 1;
+  }
+  if (flagS && (oldSimb == '\n' || numberSimbol == 1)) {
+    if (simb != '\n' && examS) {
+      printf("%6.d\t", *number);
+      *number = *number + 1;
+    }
+  }
+}
+
+void funcFlagE(int flagS, char simb, int examS) {
+  if (!flagS && simb == '\n') {
+    printf("$");
+  } else if (examS && simb == '\n') {
+    printf("$");
+  }
+}
+
+void funcFlagT(char simb, int *examT) {
+  if (simb == 9) *examT = 1;
+}
+
+void funcFlagV(char simb, int *examV) {
+  if (simb >= 0 && simb != 9 && simb != 10 && simb < 32) {
+    printf("^%c", simb + 64);
+    *examV = 1;
+  } else if (simb == 127) {
+    printf("^?");
+    *examV = 1;
+  }
+}
+
+void outputFuncCat(int flagS, int *examS, int *examV, int *examT, char simb) {
+  if (flagS && *examS) {
+    if (*examT) {
+      printf("^I");
+      *examS = 0;
+      *examT = 0;
+    } else if (*examV) {
+      *examV = 0;
+    } else {
+      printf("%c", simb);
+      *examS = 0;
+    }
+  } else if (!flagS) {
+    if (*examT) {
+      printf("^I");
+      *examT = 0;
+    } else if (*examV) {
+      *examV = 0;
+    } else {
+      printf("%c", simb);
+    }
+  }
 }
